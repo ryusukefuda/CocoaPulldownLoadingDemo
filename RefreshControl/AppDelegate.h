3c81aeeebe94af2f9d2387bb675b5a87154f2e7b
ryusukefuda <ryukai_f@yahoo.co.jp>
2014-04-08 10:23:59 +0900

    Initial Commit

---
+++ RefreshControl/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RefreshControl
+//
+//  Created by RyusukeFuda on 2014/04/08.
+//  Copyright (c) 2014年 RyusukeFuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RefreshControl/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RefreshControl
+//
+//  Created by RyusukeFuda on 2014/04/08.
+//  Copyright (c) 2014年 RyusukeFuda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
